40973a31d96f1292e0f516b009a90794ff5bfd48
Salim <Salim@Salim-PC>
2012-12-09 14:16:09 +0600

    my first testing zf 2 site..

--- module/Album/src/Album/Model/AlbumTable.php
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -1,26 +1,24 @@
 <?php
+
 namespace Album\Model;
 
 use Zend\Db\TableGateway\TableGateway;
 
-class AlbumTable
-{
+class AlbumTable {
+
     protected $tableGateway;
 
-    public function __construct(TableGateway $tableGateway)
-    {
+    public function __construct(TableGateway $tableGateway) {
         $this->tableGateway = $tableGateway;
     }
 
-    public function fetchAll()
-    {
+    public function fetchAll() {
         $resultSet = $this->tableGateway->select();
         return $resultSet;
     }
 
-    public function getAlbum($id)
-    {
-        $id  = (int) $id;
+    public function getAlbum($id) {
+        $id = (int) $id;
         $rowset = $this->tableGateway->select(array('id' => $id));
         $row = $rowset->current();
         if (!$row) {
@@ -29,14 +27,13 @@ class AlbumTable
         return $row;
     }
 
-    public function saveAlbum(Album $album)
-    {
+    public function saveAlbum(Album $album) {
         $data = array(
             'artist' => $album->artist,
-            'title'  => $album->title,
+            'title' => $album->title,
         );
 
-        $id = (int)$album->id;
+        $id = (int) $album->id;
         if ($id == 0) {
             $this->tableGateway->insert($data);
         } else {
@@ -48,8 +45,8 @@ class AlbumTable
         }
     }
 
-    public function deleteAlbum($id)
-    {
+    public function deleteAlbum($id) {
         $this->tableGateway->delete(array('id' => $id));
     }
+
 }
