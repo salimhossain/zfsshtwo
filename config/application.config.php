bcaca7778f2c5e16a71c3face7f8deb72a566789
Salim <Salim@Salim-PC>
2012-12-10 21:35:19 +0600

    my first testing zf 2 site..

--- config/application.config.php
+++ config/application.config.php
@@ -1,8 +1,8 @@
 <?php
 return array(
     'modules' => array(
-        'Application',
         'Album',
+        'Application',
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
